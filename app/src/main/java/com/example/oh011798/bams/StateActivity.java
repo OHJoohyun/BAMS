6ccd32515877968a7b8824b9bad1473f9929ebd7
kanghrrr <hrkwang@gmail.com>
2015-11-29 20:16:29 +0900

    151129

--- app/src/main/java/com/example/oh011798/bams/StateActivity.java
+++ app/src/main/java/com/example/oh011798/bams/StateActivity.java
@@ -1,30 +1,216 @@
 package com.example.oh011798.bams;
 
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.perples.recosdk.RECOBeacon;
+import com.perples.recosdk.RECOBeaconManager;
+import com.perples.recosdk.RECOBeaconRegion;
+import com.perples.recosdk.RECOBeaconRegionState;
+import com.perples.recosdk.RECOErrorCode;
+import com.perples.recosdk.RECOMonitoringListener;
+import com.perples.recosdk.RECORangingListener;
+import com.perples.recosdk.RECOServiceConnectListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 /**
  * Created by 강동현 on 2015-11-25.
  */
-public class StateActivity extends AppCompatActivity implements Runnable {
+public class StateActivity extends AppCompatActivity implements Runnable, RECOServiceConnectListener, RECORangingListener {
 
+    public static final String RECO_UUID = "24DDF411-8CF1-440C-87CD-E368DAF9C93E";
+    /**
+     * SCAN_RECO_ONLY:
+     *
+     * If true, the application scans RECO beacons only, otherwise it scans all beacons.
+     * It will be used when the instance of RECOBeaconManager is created.
+     *
+     * true일 경우 레코 비콘만 스캔하며, false일 경우 모든 비콘을 스캔합니다.
+     * RECOBeaconManager 객체 생성 시 사용합니다.
+     */
+    public static final boolean SCAN_RECO_ONLY = true;
+    /**
+     * ENABLE_BACKGROUND_RANGING_TIMEOUT:
+     *
+     * If true, the application stops to range beacons in the entered region automatically in 10 seconds (background),
+     * otherwise it continues to range beacons. (It affects the battery consumption.)
+     * It will be used when the instance of RECOBeaconManager is created.
+     *
+     * 백그라운드 ranging timeout을 설정합니다.
+     * true일 경우, 백그라운드에서 입장한 region에서 ranging이 실행 되었을 때, 10초 후 자동으로 정지합니다.
+     * false일 경우, 계속 ranging을 실행합니다. (배터리 소모율에 영향을 끼칩니다.)
+     * RECOBeaconManager 객체 생성 시 사용합니다.
+     */
+    public static final boolean ENABLE_BACKGROUND_RANGING_TIMEOUT = true;
+    /**
+     * DISCONTINUOUS_SCAN:
+     *
+     * There is a known android bug that some android devices scan BLE devices only once.
+     * (link: http://code.google.com/p/android/issues/detail?id=65863)
+     * To resolve the bug in our SDK, you can use setDiscontinuousScan() method of the RECOBeaconManager.
+     * This method is to set whether the device scans BLE devices continuously or discontinuously.
+     * The default is set as FALSE. Please set TRUE only for specific devices.
+     *
+     * 일부 안드로이드 기기에서 BLE 장치들을 스캔할 때, 한 번만 스캔 후 스캔하지 않는 버그(참고: http://code.google.com/p/android/issues/detail?id=65863)가 있습니다.
+     * 해당 버그를 SDK에서 해결하기 위해, RECOBeaconManager에 setDiscontinuousScan() 메소드를 이용할 수 있습니다.
+     * 해당 메소드는 기기에서 BLE 장치들을 스캔할 때(즉, ranging 시에), 연속적으로 계속 스캔할 것인지, 불연속적으로 스캔할 것인지 설정하는 것입니다.
+     * 기본 값은 FALSE로 설정되어 있으며, 특정 장치에 대해 TRUE로 설정하시길 권장합니다.
+     */
+    public static final boolean DISCONTINUOUS_SCAN = false;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_LOCATION = 10;
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private View mLayout;
     ProgressBar progressBar;
     int progress = 0;
     Thread thread;
+
+    private RECOBeaconManager recoManager;
+    private ArrayList<RECOBeaconRegion> rangingRegions;
+
+    // RecoBackgroundMonitoringService bms = new RecoBackgroundMonitoringService();
+    RecoBackgroundRangingService brs = new RecoBackgroundRangingService();
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        mLayout=findViewById(R.id.stateLayout);
 
+        mBluetoothManager=(BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter=mBluetoothManager.getAdapter();
+
+        if(mBluetoothAdapter==null || !mBluetoothAdapter.isEnabled()){
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent,REQUEST_ENABLE_BT);
+        }
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                Log.i("MainActivity", "The location permission (ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION) is not granted.");
+                this.requestLocationPermission();
+            }
+            else{
+                Log.i("MainActivity", "The location permission (ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION) is already granted.");
+
+            }
+        }
+
+        recoManager = RECOBeaconManager.getInstance(this,true,true);
+        recoManager.bind(this);
+        recoManager.setRangingListener(this);
+
+    }
+
+    @Override
+    public void onServiceConnect(){
+        Toast toast = Toast.makeText(this,"연결되었습니다",Toast.LENGTH_SHORT);
+        toast.show();
+
+        rangingRegions=new ArrayList<RECOBeaconRegion>();
+
+        rangingRegions.add(new RECOBeaconRegion("24DDF411-8CF1-440C-87CD-E368DAF9C93E", 501, "mine"));
+
+        for(RECOBeaconRegion region : rangingRegions){
+            try{
+                recoManager.startRangingBeaconsInRegion(region);
+                recoManager.requestStateForRegion(region);
+            } catch(RemoteException e){
+                // error occursion code
+            } catch(NullPointerException e){
+                // NullPointerException occursion code
+            }
+        }
+    }
+
+    @Override
+    public void onServiceFail(RECOErrorCode arg0){
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void didRangeBeaconsInRegion(Collection<RECOBeacon> arg0,RECOBeaconRegion arg1){
+
+        if(arg0.size()==0){
+            Toast toast = Toast.makeText(this,"gone",Toast.LENGTH_SHORT);
+            toast.show();
+        } else{
+            Toast toast = Toast.makeText(this,"still here",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        if(arg1.getMajor()==501){
+            if(arg0.size()==0){
+                Toast toast = Toast.makeText(this,"gone",Toast.LENGTH_SHORT);
+                toast.show();
+            } else{
+                Toast toast = Toast.makeText(this,"still here",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+    }
+
+    @Override
+    public void rangingBeaconsDidFailForRegion(RECOBeaconRegion arg0,RECOErrorCode arg1){
+        Toast toast = Toast.makeText(this,"failed",Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     public void start(View v) {
         thread = new Thread(this);
         thread.start();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            //If the request to turn on bluetooth is denied, the app will be finished.
+            //사용자가 블루투스 요청을 허용하지 않았을 경우, 어플리케이션은 종료됩니다.
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch(requestCode) {
+            case REQUEST_LOCATION : {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(mLayout, R.string.location_permission_granted, Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(mLayout, R.string.location_permission_not_granted, Snackbar.LENGTH_LONG).show();
+                }
+            }
+            default :
+                break;
+        }
+
+
+    }
+
 
     @Override
     public void run() {
@@ -39,5 +225,49 @@ public class StateActivity extends AppCompatActivity implements Runnable {
             }
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void requestLocationPermission() {
+        if(!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+            return;
+        }
+
+        Snackbar.make(mLayout, R.string.location_permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ActivityCompat.requestPermissions(StateActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+                    }
+                })
+                .show();
+    }
+
+    /*
+    private boolean isBackgroundMonitoringServiceRunning(Context context) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo runningService : am.getRunningServices(Integer.MAX_VALUE)) {
+            if(RecoBackgroundMonitoringService.class.getName().equals(runningService.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isBackgroundRangingServiceRunning(Context context) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo runningService : am.getRunningServices(Integer.MAX_VALUE)) {
+            if(RecoBackgroundRangingService.class.getName().equals(runningService.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    */
 }
 
